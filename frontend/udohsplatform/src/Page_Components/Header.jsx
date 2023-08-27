03763f0458a0af8f8d8a34142faefb1cab6f378b
Udoh <udohabasionosunday@gmail.com>
2023-08-27 01:59:17 +0100

    Added the Sign_in pop up that will show when sign in button is clicked

--- frontend/udohsplatform/src/Page_Components/Header.jsx
+++ frontend/udohsplatform/src/Page_Components/Header.jsx
@@ -10,6 +10,7 @@ import { RiChatHistoryLine } from "react-icons/ri";
 import { useEffect, useState } from "react";
 import { useMatchMedia } from "../customHooks/useMatchMedia";
 import HeaderForBigScreen from "./HeaderForBigScreen";
+import Sign_In from "./sign_in";
 
 const Header = () => {
   const [user, setUser] = useState(false);
@@ -57,173 +58,182 @@ const Header = () => {
   const [formHasFocus, setFormHasFocus] = useState(false);
 
   return (
-    <header>
-      {smallScreenNav ? (
-        <nav>
-          <div className="flex items-center justify-between dark:bg-[#242424] bg-white p-4 fixed w-full z-10">
-            <Link
-              to="/"
-              className="font-bold tracking-[-0.12em] text-2xl"
-              id="logo"
-            >
-              udohsplatform
-            </Link>
+    <>
+      <header>
+        {smallScreenNav ? (
+          <nav>
+            <div className="flex items-center justify-between dark:bg-[#242424] bg-white p-4 fixed w-full z-10">
+              <Link
+                to="/"
+                className="font-bold tracking-[-0.12em] text-2xl"
+                id="logo"
+              >
+                udohsplatform
+              </Link>
 
-            <div className="">
-              {!formHasFocus && (
-                <button
-                  type="button"
-                  aria-label="search"
-                  title="Search"
-                  className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer peer"
-                  onFocus={() => giveSearchFieldFocus()}
-                >
-                  <BiSearchAlt />
-                </button>
-              )}
+              <div className="">
+                {!formHasFocus && (
+                  <button
+                    type="button"
+                    aria-label="search"
+                    title="Search"
+                    className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer peer"
+                    onFocus={() => giveSearchFieldFocus()}
+                  >
+                    <BiSearchAlt />
+                  </button>
+                )}
 
-              {formHasFocus && (
-                <button
-                  type="button"
-                  aria-label="search"
-                  title="Search"
-                  className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer "
-                  onClick={() => {
-                    document.activeElement.blur();
-                  }}
-                >
-                  <AiOutlineClose />
-                </button>
-              )}
+                {formHasFocus && (
+                  <button
+                    type="button"
+                    aria-label="search"
+                    title="Search"
+                    className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer "
+                    onClick={() => {
+                      document.activeElement.blur();
+                    }}
+                  >
+                    <AiOutlineClose />
+                  </button>
+                )}
 
-              <div className="absolute -left-[1800px] top-16 w-full focus-within:left-0 peer-focus:left-0 transition-all duration-500">
-                <form
-                  className="w-full flex justify-center"
-                  onSubmit={(e) => e.preventDefault()}
-                >
-                  <div className="relative w-full max-w-[450px]">
-                    <input
-                      id="search-input-field"
-                      type="search"
-                      placeholder="search..."
-                      className="w-full block border-black dark:border-white border-2 rounded-3xl h-10"
-                      onFocus={() => setFormHasFocus(true)}
-                      onBlur={() => setFormHasFocus(false)}
-                    />
-                    <button
-                      type="submit"
-                      aria-label="Submit"
-                      title="Search"
-                      className="text-2xl absolute right-1 top-2"
-                    >
-                      <BiSearchAlt />
-                    </button>
-                  </div>
-                </form>
+                <div className="absolute -left-[1800px] top-16 w-full focus-within:left-0 peer-focus:left-0 transition-all duration-500">
+                  <form
+                    className="w-full flex justify-center"
+                    onSubmit={(e) => e.preventDefault()}
+                  >
+                    <div className="relative w-full max-w-[450px]">
+                      <input
+                        id="search-input-field"
+                        type="search"
+                        placeholder="search..."
+                        className="w-full block border-black dark:border-white border-2 rounded-3xl h-10"
+                        onFocus={() => setFormHasFocus(true)}
+                        onBlur={() => setFormHasFocus(false)}
+                      />
+                      <button
+                        type="submit"
+                        aria-label="Submit"
+                        title="Search"
+                        className="text-2xl absolute right-1 top-2"
+                      >
+                        <BiSearchAlt />
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
+
+              <button
+                aria-label="menu-button-toggle"
+                type="button"
+                title="Menu"
+                className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer"
+                onClick={() => {
+                  const theCart = document.querySelector("#menu-small-screen");
+                  theCart.classList.toggle("hidden");
+
+                  setTimeout(() => {
+                    theCart.classList.toggle("translate-y-[-700px]");
+                    theCart.classList.toggle("translate-y-[20px]");
+                  }, 0.001);
+
+                  togglePageScrolling();
+                }}
+              >
+                <GiHamburgerMenu />
+              </button>
             </div>
 
-            <button
-              aria-label="menu-button-toggle"
-              type="button"
-              title="Menu"
-              className="text-3xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer"
-              onClick={() => {
-                const theCart = document.querySelector("#menu-small-screen");
-                theCart.classList.toggle("hidden");
-
-                setTimeout(() => {
-                  theCart.classList.toggle("translate-y-[-700px]");
-                  theCart.classList.toggle("translate-y-[20px]");
-                }, 0.001);
-
-                togglePageScrolling();
-              }}
-            >
-              <GiHamburgerMenu />
-            </button>
-          </div>
-
-          <div
-            id="menu-small-screen"
-            className="fixed overflow-auto bg-white dark:bg-[#242424] w-full h-full mt-[-16px] p-8 dark:text-[#fcffba] font-bold 
+            <div
+              id="menu-small-screen"
+              className="fixed overflow-auto bg-white dark:bg-[#242424] w-full h-full mt-[-16px] p-8 dark:text-[#fcffba] font-bold 
              transition-all ease-linear duration-[500ms] hidden translate-y-[-700px] z-10
             "
-          >
-            <ul className="flex flex-col items-center justify-around h-full text-xl">
-              <li className="w-full" onClick={toggleOffMenu}>
-                <Link
-                  to="/"
-                  className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
-                >
-                  Our story <RiChatHistoryLine className="ml-2" />
-                </Link>
-              </li>
-              <li className="w-full" onClick={toggleOffMenu}>
-                <Link
-                  to="/"
-                  className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
-                >
-                  Membership <MdOutlineVerifiedUser className="ml-2" />
-                </Link>
-              </li>
-              <li className="w-full" onClick={toggleOffMenu}>
-                <Link
-                  to="/"
-                  className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
-                >
-                  Write
-                  <BsPencilSquare className="ml-2" />
-                </Link>
-              </li>
-
-              {!user && (
-                <>
-                  <li className="w-full" onClick={toggleOffMenu}>
-                    <Link
-                      to="/"
-                      className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
-                    >
-                      Sign in <HiOutlineLogin className="ml-2" />
-                    </Link>
-                  </li>
-                  <li className="w-full" onClick={toggleOffMenu}>
-                    <Link
-                      to="/"
-                      className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
-                    >
-                      Get started <AiOutlineLogin className="ml-2" />
-                    </Link>
-                  </li>
-                </>
-              )}
-
-              {user && (
+            >
+              <ul className="flex flex-col items-center justify-around h-full text-xl">
                 <li className="w-full" onClick={toggleOffMenu}>
                   <Link
                     to="/"
                     className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
                   >
-                    Sign out <HiOutlineLogout className="ml-2" />
+                    Our story <RiChatHistoryLine className="ml-2" />
                   </Link>
                 </li>
-              )}
+                <li className="w-full" onClick={toggleOffMenu}>
+                  <Link
+                    to="/"
+                    className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
+                  >
+                    Membership <MdOutlineVerifiedUser className="ml-2" />
+                  </Link>
+                </li>
+                <li className="w-full" onClick={toggleOffMenu}>
+                  <Link
+                    to="/"
+                    className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
+                  >
+                    Write
+                    <BsPencilSquare className="ml-2" />
+                  </Link>
+                </li>
 
-              <button
-                aria-label="menu"
-                type="button"
-                className="text-5xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer"
-                onClick={toggleOffMenu}
-              >
-                <AiOutlineClose />
-              </button>
-            </ul>
-          </div>
-        </nav>
-      ) : (
-        <HeaderForBigScreen user={user} />
-      )}
-    </header>
+                {!user && (
+                  <>
+                    <li className="w-full" onClick={toggleOffMenu}>
+                      <Link
+                        to="/"
+                        className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
+                      >
+                        Sign in <HiOutlineLogin className="ml-2" />
+                      </Link>
+                    </li>
+                    <li className="w-full" onClick={toggleOffMenu}>
+                      <Link
+                        to="/"
+                        className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
+                      >
+                        Get started <AiOutlineLogin className="ml-2" />
+                      </Link>
+                    </li>
+                  </>
+                )}
+
+                {user && (
+                  <li className="w-full" onClick={toggleOffMenu}>
+                    <Link
+                      to="/"
+                      className="border-2 bg-[#70dbb8] dark:bg-[#242424] dark:border-[#fcffba] w-full text-center p-2 rounded-2xl shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px] flex items-center justify-center"
+                    >
+                      Sign out <HiOutlineLogout className="ml-2" />
+                    </Link>
+                  </li>
+                )}
+
+                <button
+                  aria-label="menu"
+                  type="button"
+                  className="text-5xl text-[#af4261] dark:text-[#a1d06d] cursor-pointer"
+                  onClick={toggleOffMenu}
+                >
+                  <AiOutlineClose />
+                </button>
+              </ul>
+            </div>
+          </nav>
+        ) : (
+          <HeaderForBigScreen user={user} />
+        )}
+      </header>
+
+      <section
+        id="sign_in"
+        className="fixed overflow-auto w-full h-full z-10 bg-[rgb(255,255,255,.95)] dark:bg-[rgb(0,0,0,.95)] hidden"
+      >
+        <Sign_In />
+      </section>
+    </>
   );
 };
 
---
+++ frontend/udohsplatform/src/Page_Components/sign_in.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { FaRegWindowClose } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import SignUpWithGoogle from "./signupWithGoogle";
+import { HiOutlineLogin } from "react-icons/hi";
+
+const Sign_In = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="flex justify-center">
+      <div className="bg-white dark:bg-[#242424] h-full fixed w-full overflow-auto max-w-[700px] min-[600px]:p-6 shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
+        <section className="p-4">
+          <h2 className="text-center text-3xl text-[#81ba40] dark:text-[#70dbb8] font-bold">
+            Welcome back.
+          </h2>
+
+          <form onSubmit={(e) => e.preventDefault()} className="mt-8">
+            <div className="flex flex-col-reverse mb-8 relative mt-16">
+              <input
+                type="email"
+                required
+                placeholder=" "
+                id="email"
+                className="h-10 rounded-xl ring-2 ring-[#81ba40] dark:ring-[#70dbb8] p-1 peer"
+              />
+
+              <label
+                htmlFor="email"
+                className="cursor-text text-xl p-1 absolute peer-placeholder-shown:top-[50%] peer-placeholder-shown:translate-y-[-50%] peer-focus:top-[-90%] peer-focus:translate-y-[0] top-[-90%] transition-all duration-500 ease-linear"
+              >
+                Email&nbsp;<span className="text-red-500">&#42;</span>
+              </label>
+            </div>
+
+            <div className="flex flex-col-reverse relative mt-20">
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                required
+                placeholder=" "
+                className="h-10 rounded-xl ring-2 ring-[#81ba40] dark:ring-[#70dbb8] p-1 peer"
+              />
+
+              <button
+                type="button"
+                className="absolute top-1 right-0 cursor-pointer text-3xl text-[#81ba40] dark:text-[#70dbb8] "
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </button>
+
+              <label
+                htmlFor="password"
+                className="cursor-text text-xl p-1 absolute peer-placeholder-shown:top-[50%] peer-placeholder-shown:translate-y-[-50%] peer-focus:top-[-90%] peer-focus:translate-y-[0] top-[-90%] transition-all duration-500 ease-linear"
+              >
+                Password&nbsp;<span className="text-red-500">&#42;</span>
+              </label>
+            </div>
+
+            <div className="mb-12 text-right mt-3 text-lg text-[#81ba40] dark:text-[#70dbb8] font-bold">
+              <button type="button" className="underline">
+                Forgot your password&#x3f;
+              </button>
+            </div>
+
+            <button
+              type="submit"
+              disabled={false}
+              className="w-full font-bold uppercase relative flex items-center justify-center px-6 py-3 text-lg tracking-tighter text-white bg-gray-800 rounded-md group"
+            >
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-black dark:bg-white rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-[#81ba40] dark:bg-[#70dbb8] rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-black dark:bg-white rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-black transition-colors duration-200 ease-in-out delay-100 group-hover:text-white dark:group-hover:text-black flex items-center">
+                Sign in <HiOutlineLogin className="ml-2" />
+              </span>
+            </button>
+          </form>
+
+          <div className="mt-6 text-center text-[#81ba40] dark:text-[#70dbb8] underline text-lg">
+            <Link>Don&apos;t have an account&#x3f; Register</Link>
+          </div>
+
+          <SignUpWithGoogle />
+
+          <button type="button" className="text-4xl absolute top-2 right-3">
+            <FaRegWindowClose />
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Sign_In;
