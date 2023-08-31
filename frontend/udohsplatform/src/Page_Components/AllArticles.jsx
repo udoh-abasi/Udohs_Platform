76cc01e8f86acdc3ee194566f8657871e5f740fd
Udoh <udohabasionosunday@gmail.com>
2023-08-31 06:02:40 +0100

    Added more frontend design to the AllArticles page

--- frontend/udohsplatform/src/Page_Components/AllArticles.jsx
+++ frontend/udohsplatform/src/Page_Components/AllArticles.jsx
@@ -7,13 +7,11 @@ const AllArticles = () => {
   return (
     <main className="min-h-screen pt-[90px] flex justify-center">
       <div className="max-w-[750px]">
-        <section className="text-xl min-[500px]:text-2xl flex justify-between mb-4  p-4">
+        <section className="min-[400px]:text-xl min-[500px]:text-2xl flex justify-between mb-4  p-4">
           <button
-            className={`${
-              postType == "mostRecent"
-                ? "border-[#81ba40] dark:border-[#70dbb8] font-bold"
-                : "border-b-transparent"
-            } border-b-2 `}
+            className={`px-4 rounded-br-xl rounded-tl-xl py-2 ring-[#81ba40] dark:ring-[#70dbb8] hover:bg-[#81ba40] dark:hover:bg-[#70dbb8] hover:text-white dark:hover:text-black transition-all duration-300 ease-linear shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]'
+            ${postType == "mostRecent" ? "ring-4 font-bold" : "ring-1"}
+            `}
             type="button"
             onClick={() => {
               setPostType("mostRecent");
@@ -23,11 +21,9 @@ const AllArticles = () => {
           </button>
 
           <button
-            className={`${
-              postType == "mostViewed"
-                ? "border-[#81ba40] dark:border-[#70dbb8]  font-bold"
-                : "border-b-transparent"
-            } border-b-2 `}
+            className={`px-4 rounded-br-xl rounded-tl-xl py-2 ring-[#81ba40] dark:ring-[#70dbb8] hover:bg-[#81ba40] dark:hover:bg-[#70dbb8] hover:text-white dark:hover:text-black transition-all duration-300 ease-linear shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]'
+            ${postType == "mostViewed" ? "ring-4 font-bold" : "ring-1"}
+            `}
             type="button"
             onClick={() => {
               setPostType("mostViewed");
@@ -37,18 +33,44 @@ const AllArticles = () => {
           </button>
         </section>
 
-        <section className="my-8 font-bold border-b-4 border-[#81ba40] dark:border-[#70dbb8]  pb-8 px-4">
-          <label htmlFor="filter" className="mr-4 text-lg min-[500px]:text-xl">
-            Sort by
-          </label>
+        <section className="my-8 font-bold border-b-4 border-[#81ba40] dark:border-[#70dbb8]  pb-8 px-4 flex justify-between min-[500px]:gap-16">
+          <div className="max-[512px]:flex flex-col">
+            <label
+              htmlFor="filter"
+              className="mr-4 text-lg min-[500px]:text-xl"
+            >
+              Sort by
+            </label>
 
-          <select
-            id="filter"
-            className="w-[100px] min-[500px]:text-lg rounded-xl ring-2 ring-[#81ba40] dark:ring-[#70dbb8] p-1"
-          >
-            <option value="title">Title</option>
-            <option value="author">Author</option>
-          </select>
+            <select
+              id="filter"
+              className="w-[100px] min-[500px]:text-lg rounded-xl ring-2 ring-[#81ba40] dark:ring-[#70dbb8] p-1"
+            >
+              <option value="title" selected>
+                Title
+              </option>
+              <option value="author">Author</option>
+            </select>
+          </div>
+
+          <div className="max-[512px]:flex flex-col">
+            <label
+              htmlFor="filter"
+              className="mr-4 text-lg min-[500px]:text-xl"
+            >
+              Order
+            </label>
+
+            <select
+              id="filter"
+              className="min-[500px]:w-[140px] w-[120px] min-[500px]:text-lg rounded-xl ring-2 ring-[#81ba40] dark:ring-[#70dbb8] p-1"
+            >
+              <option value="title" selected>
+                Ascending
+              </option>
+              <option value="author">Descending</option>
+            </select>
+          </div>
         </section>
 
         <section className="p-4 mt-8 items-center flex gap-8 max-[430px]:gap-0 shadow-[0px_5px_15px_rgba(0,0,0,0.35)] dark:shadow-[rgba(255,255,255,0.089)_0px_0px_7px_5px]">
--- frontend/udohsplatform/src/Page_Components/HomePage.jsx
+++ frontend/udohsplatform/src/Page_Components/HomePage.jsx
@@ -312,7 +312,7 @@ const HomePage = () => {
       <div className="flex justify-center">
         <section className="p-4 flex-[0_1_600px] my-10">
           <Link
-            href="#_"
+            to="/allArticles"
             className="relative inline-flex items-center justify-center py-3 pl-4 pr-12 overflow-hidden text-white dark:text-black transition-all duration-150 ease-in-out rounded-2xl hover:pl-10 hover:pr-6 bg-[#af4261] dark:bg-[#70dbb8] font-bold group w-full "
           >
             <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-black group-hover:h-full"></span>
