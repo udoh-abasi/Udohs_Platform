f3c12e2b6ef4fdc68b282c8fcc0bfd5f3e76a6a8
Udoh <udohabasionosunday@gmail.com>
2023-10-26 02:14:43 +0100

    Added Admin for Reactions

--- backend/udohsplatform/reactions/admin.py
+++ backend/udohsplatform/reactions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Reactions
 
-# Register your models here.
+
+class ReactionsAdmin(admin.ModelAdmin):
+    list_display = ("user", "article", "reaction_type")
+
+
+admin.site.register(Reactions, ReactionsAdmin)
